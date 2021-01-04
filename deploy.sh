7d2c39ffd98a8ca82e172c4106bab6299ab835d9
Felippe <felippe.moraes@zoho.com>
2021-01-04 04:43:10 -0300

    new deployer version

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 git checkout gh-pages
 git reset --hard origin/master
 
+npm install
 npm run build
+
 find * -maxdepth 0 -name 'build' -prune -o -exec rm -rf '{}' ';'
 mv ./build/* .
 
-git rm -rf --cache .
 git add .
 git commit -m "deploy"
 
