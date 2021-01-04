09ba0d8c1fae9cdfefb24254fb58fb2b6b773d16
Felippe <felippe.moraes@zoho.com>
2021-01-04 04:46:39 -0300

    deploy

--- src/i18n/locales/index.tsx
+++
@@ -1,7 +0,0 @@
-import en from './en.tsx'
-import ptBR from './pt-BR.tsx'
-
-export default {
-  'en': en,
-  'pt-BR': ptBR
-}
--- src/views/Home/home.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import React from 'react'
-import Home from './'
-
-test('renders learn react link', () => {
-  render(<Home />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
